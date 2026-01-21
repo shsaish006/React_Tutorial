eed1c078456466d2e30e1cb5b03c58e966d1d577
Shivam Kumar Singh <shsaish006@gmail.com>
2026-01-21 13:10:11 +0530

    ADD CODE

--- Counter.js
+++ Counter.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, reset } from './store'; // import from store.js
+import { increment, decrement, reset } from '../features/counterSlice';
 
 export default function Counter() {
   const count = useSelector(state => state.counter);
   const dispatch = useDispatch();
 
   return (
-    <div style={{ textAlign: 'center', marginTop: 50 }}>
-      <h2>Redux Counter: {count}</h2>
+    <div style={{ textAlign: 'center' }}>
+      <h3>Counter: {count}</h3>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(reset())} style={{ margin: '0 10px' }}>Reset</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
       <button onClick={() => dispatch(increment())}>Increment</button>
     </div>
   );
